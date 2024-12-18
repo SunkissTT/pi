0669b1a9790ec6968d51a63e314463d5886e7381
SunkissTT <eownslsla@gmail.com>
2024-12-18 10:05:46 +0900

    Remove all files from repository

--- sss.py
+++
@@ -1,8 +0,0 @@
-import ollama
-
-def Qa(query):
-    #query = input('질문을 영어로 입력하세요 : ')
-    response = ollama.chat(model='llama3.2:1b', messages=[{'role': 'user','content': query+'. give me a brief answer.',},])
-    answer = response['message']['content']
-    #print(answer)
-    return answer
